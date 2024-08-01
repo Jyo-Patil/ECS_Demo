cccf6c332c247622204ead1795a1c404e2e20681
Jyoti.Patil <jyoti.patil@avon.com>
2024-08-01 13:11:41 +0530

    added code for ECS

---
+++ devops/backend.tf
@@ -0,0 +1,11 @@
+
+terraform {
+  backend "s3" {
+    bucket = "ecsbackendbucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "ecstable"
+    }
+  }
+
